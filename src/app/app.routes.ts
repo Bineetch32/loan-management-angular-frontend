06f0fab819686df43e2542a0494c16eb8b3fba7e
BineetChauhan <115210882+Bineetch32@users.noreply.github.com>
2026-09-19 19:09:18 +0530

    Add documents route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import {Routes} from '@angular/router';
-import {LoginComponent} from './login.component';
-import {DashboardComponent} from './dashboard.component';
-import {LoanFormComponent} from './loan-form.component';
-import {LoansComponent} from './loans.component';
-import {EmiComponent} from './emi.component';
-export const routes:Routes=[
-{path:'login',component:LoginComponent},{path:'dashboard',component:DashboardComponent},
-{path:'loans',component:LoansComponent},{path:'loan/new',component:LoanFormComponent},
-{path:'loan/edit/:id',component:LoanFormComponent},{path:'emi',component:EmiComponent},
-{path:'',redirectTo:'login',pathMatch:'full'},{path:'**',redirectTo:'login'}];
+import {LoginComponent} from './login.component';import {DashboardComponent} from './dashboard.component';import {LoanFormComponent} from './loan-form.component';import {LoansComponent} from './loans.component';import {EmiComponent} from './emi.component';import {DocumentsComponent} from './documents.component';
+export const routes:Routes=[{path:'login',component:LoginComponent},{path:'dashboard',component:DashboardComponent},{path:'loans',component:LoansComponent},{path:'loan/new',component:LoanFormComponent},{path:'loan/edit/:id',component:LoanFormComponent},{path:'documents/:loanId',component:DocumentsComponent},{path:'emi',component:EmiComponent},{path:'',redirectTo:'login',pathMatch:'full'},{path:'**',redirectTo:'login'}];
